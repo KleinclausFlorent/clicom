5877256d66e28461cb9b1b32e49f8a03e289d987
Kleinclaus <kleinclaus.florent@gmail.com>
2020-05-21 05:26:20 +0200

    page commande produit moins vierge

--- php/vueCommande.php
+++ php/vueCommande.php
@@ -1,1 +1,14 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Commande</title>
+    <link rel="stylesheet" href="../css/styleUser.css" id="stylesheet">
+    <script src="../js/Set-css_script.js"></script>
+</head>
+<body>
+    <?php include("managerCommande.php"); ?>
+    <h1>Commandes</h1>
+    <p><a href="Consultation.php">Accèder aux menu</a></p>
+</body>
+</html>
--- php/vueProduit.php
+++ php/vueProduit.php
@@ -1,1 +1,14 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Produit</title>
+    <link rel="stylesheet" href="../css/styleUser.css" id="stylesheet">
+    <script src="../js/Set-css_script.js"></script>
+</head>
+<body>
+<?php include("managerProduit.php"); ?>
+<h1>Produits</h1>
+<p><a href="Consultation.php">Accèder aux menu</a></p>
+</body>
+</html>
